783ccceaa9074030d6985725acbaadff47b6749c
Harry <harry.n.griffiths@gmail.com>
2024-02-21 19:10:01 +0000

    B1-B3 complete

--- src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
+++ src/Renderer/DrawBlock/SheetBeautifyHelpers.fs
@@ -1,2 +1,39 @@
 ﻿module SheetBeautifyHelpers
 
+open CommonTypes.JSONComponent
+open DrawModelType.SymbolT
+open CommonTypes
+open Optics
+open Optics.Optic
+
+//B1R
+let getCustomSymbolDimension (sym:Symbol) =
+    let comp = get component_ sym
+    (Option.defaultValue 1.0 sym.HScale * get w_ comp,
+     Option.defaultValue 1.0 sym.VScale * get h_ comp)
+
+//B1W
+let setCustomSymbolDimension (width, height) (sym:Symbol) =
+    let comp = get component_ sym
+    let newComp = set w_ width comp
+                |> set h_ height
+    set component_ comp sym
+
+/// Returns the dimension of a symbol as a tuple (width * height)
+let customSymbolDimension_ = Lens.create (getCustomSymbolDimension) (setCustomSymbolDimension)
+
+//B2W
+let setSymbolPosition (position:XYPos) (sym:Symbol) =
+    set posOfSym_ position sym
+
+let getSymbolPortsOnSide side (sym:Symbol) =
+    let pMap = get portMaps_ sym
+    get order_ pMap
+    |> Map.tryFind side
+
+let setSymbolPortsOnSide side ports sym =
+    let pMap = get portMaps_ sym
+    let orderMap = get order_ pMap
+                   |> Map.add side ports
+    set portMaps_ (set order_ orderMap pMap) sym
+
