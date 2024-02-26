766745f558570d6916c3d510ff68c63393585551
Ben Marconi <ben1marconi@gmail.com>
2024-02-26 15:59:14 +0000

    Added Summary of Changes in RotateScale

--- src/Renderer/DrawBlock/RotateScale.fs
+++ src/Renderer/DrawBlock/RotateScale.fs
@@ -392,11 +392,49 @@ let rotateSymbolInBlock
 
 
 
+(* bam21 (worked on functions from flipSymbolInBlock to rotateBlock)
+Summary of Changes:
+    1.  In flipSymbolInBlock, I added or reformatted pipelines in most local variable and function definitions (Transform 3).  
+        Most importantly, I replaced the final block of code which was a very long pipeline: I broke it up (Transform 3) into 
+        a defined identifier that documents an intermediate value and a separate function with a clear identifier (Transform 1).
+        These changes worked towards the DUI and LFM Principles.
+        
+        Changes in Github:
+        https://github.com/ben-marconi/HLP-project/commit/98b8acb2667ed8dc740089c129f33daa8f0ed332#diff-9e7a112f68a531037835f68fd5ba2311c4d7248b2e8fd03a1a51bba8f8f99d21R441-R466
 
+    2.  In rotateBlock, I completely changed the the code of the return value of the function. I used a separate identifier to define 
+        the new model symbols. In this definition, I used a series of pipeline operators which were not originally used (Transform 3).
+        This had the effect of Reducing Syntactic Noise (RSN) and worked towards improving the DUI and LFM principles. 
+        
+        Changes in Github:
+        https://github.com/ben-marconi/HLP-project/commit/98b8acb2667ed8dc740089c129f33daa8f0ed332#diff-9e7a112f68a531037835f68fd5ba2311c4d7248b2e8fd03a1a51bba8f8f99d21R535-R544
+    
+    3.  In scaleSymbolInBlock, my most important contribution was changing the way the new center of the symbol (newCenter) was
+        calculated by abstracting the scaling operation (Transform 1) in line 493. This helped reduce repetition and improve clarity,
+        working towards the DRY, RSN and LYM principles. 
+        
+        Changes in Github
+        https://github.com/ben-marconi/HLP-project/commit/98b8acb2667ed8dc740089c129f33daa8f0ed332#diff-9e7a112f68a531037835f68fd5ba2311c4d7248b2e8fd03a1a51bba8f8f99d21R485-R492
+    
+    4.  I added XML documentation to the rotateSymbolByDegree function (DUI), which was previously missing. I added explanations about what 
+        the parameters are and what the function returns. I also renamed an identifier for further clarity (DUI).
 
-// bam21 Start
-// --------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------------------
+        Changes in Github:
+        https://github.com/ben-marconi/HLP-project/commit/98b8acb2667ed8dc740089c129f33daa8f0ed332#diff-9e7a112f68a531037835f68fd5ba2311c4d7248b2e8fd03a1a51bba8f8f99d21R501-R505
+
+Transforms Used:
+    1.  Functional Abstraction
+        a.  flipSymbolInBlock: Introduced applyFlip function to abstract flipping logic, making the main function more focused and simpler.
+        b.  rotateBlock: Abstracted the process of updating symbols after rotation into clearer, distinct steps, simplifying the overall logic.
+        c.  scaleSymbolInBlock: Abstracted the scaling operation by creating a seperate match condition
+    
+    3.  Pipelines
+        a.  flipSymbolInBlock: I broke down a very long pipeline into an identifier and a separate function. I also used pipelines in the definition 
+            of updatedTopLeft and flippedPortOrientation. 
+        b.  rotateBlock: In this function I used a separate identifier to define the new model symbols. In this definition, I used a series of 
+            pipeline operators which were not originally used and made the code much clearer in doing so.
+*)
+
 /// <summary>HLP 23: AUTHOR Ismagilov - Flip a symbol horizontally or vertically in its block.</summary>
 /// <param name="flip">  Flip horizontally or vertically</param>
 /// <param name="block"> Bounding box of selected components</param>
@@ -427,7 +465,7 @@ let flipSymbolInBlock
         currPortOrder 
         |> Map.add (flipSideHorizontal side ) sym.PortMaps.Order[side]
 
-    // Change: replace portOrder to simplify
+    // Change: identifier
     let flippedPortOrder = 
         (Map.empty, [Edge.Top; Edge.Left; Edge.Bottom; Edge.Right]) ||> List.fold flipPortList
         |> Map.map (fun edge order -> List.rev order)   
@@ -487,8 +525,8 @@ let scaleSymbolInBlock
                       | ScaleUp -> -5.0, 10.0
                       | ScaleDown -> 5.0, -10.0
 
-    let newCenterX = (block.TopLeft.X + fst scaleOffset) + ((block.W + snd scaleOffset) * xProp)
-    let newCenterY = (block.TopLeft.Y + fst scaleOffset) + ((block.H + snd scaleOffset) * yProp)
+    let newCenterX = block.TopLeft.X + fst scaleOffset + (block.W + snd scaleOffset) * xProp
+    let newCenterY = block.TopLeft.Y + fst scaleOffset + (block.H + snd scaleOffset) * yProp
     let newCenter = { X = newCenterX; Y = newCenterY }
 
     let h,w = getRotatedHAndW sym
@@ -544,7 +582,7 @@ let rotateBlock (compList:ComponentId list) (model:SymbolT.Model) (rotation:Rota
 
 // bam21 End
 // --------------------------------------------------------------------------------------------------------
-// --------------------------------------------------------------------------------------------------------
+
 
 let oneCompBoundsBothEdges (selectedSymbols: Symbol list) = 
     let maxXSymCentre = 
